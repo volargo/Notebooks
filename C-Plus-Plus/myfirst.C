d8be8533aa1d3d053c638d4020d70753411123cd
volargo <git@volargo.com>
2018-07-13 01:47:53 +0800

    add a sample C++ source file

---
+++ C-Plus-Plus/myfirst.C
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  myfirst.C
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/10/2018 10:49:48 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>								// A PREPROCESSOR directory
+
+int main(void)									// function header
+{												// start of function body
+    using namespace std;						// make definitions visible
+
+    cout << "Come up and C++ me some time.";	// messages
+    cout << endl;								// start new line
+    cout << "You won't regret it!" << endl;		// more output
+
+    return 0;									// terminate main()
+}												// end of function body
---
+++ C-Plus-Plus/spiffy.C
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  spiffy.C
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/10/2018 09:15:26 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int
+main(void)
+{
+    cout<<"Hello C++"<<endl;
+
+    cin.get();
+
+    return 0;
+}
